8339645617b09538fd46d5fa3e85d047e07a289b
CloudFlowing <13386151695@163.com>
2020-04-30 00:27:48 +0800

    Add files via upload

---
+++ MySectionClip/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MySectionClip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.copy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.replace = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.back_to_last = new System.Windows.Forms.Button();
+            this.labelStatusDescription = new System.Windows.Forms.Label();
+            this.checkFormFirst = new System.Windows.Forms.CheckBox();
+            this.controlFilter = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.OpacitySetting = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(542, 335);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // clear
+            // 
+            this.clear.AutoSize = true;
+            this.clear.Font = new System.Drawing.Font("宋体", 10F);
+            this.clear.Location = new System.Drawing.Point(566, 353);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(137, 27);
+            this.clear.TabIndex = 1;
+            this.clear.Text = "清空富文本窗口";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // copy
+            // 
+            this.copy.AutoSize = true;
+            this.copy.Font = new System.Drawing.Font("宋体", 10F);
+            this.copy.Location = new System.Drawing.Point(709, 353);
+            this.copy.Name = "copy";
+            this.copy.Size = new System.Drawing.Size(137, 27);
+            this.copy.TabIndex = 2;
+            this.copy.Text = "复制到剪贴板";
+            this.copy.UseVisualStyleBackColor = true;
+            this.copy.Click += new System.EventHandler(this.copy_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(560, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 179);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "说明:\r\n该程序是否最前展示=ctrl+q(全局热键)\r\n清空=因为用的少,删除清空的热键\r\n自动监听ctrl+c把剪贴板内容复制到富文本窗口\r\n粘贴(在程序界面" +
+    ")ctrl+x(全局热键)\r\n(注意复制是复制富文本窗口中的内容,允许编辑)\r\n使用:先ctrl+c多次复制你需要的文本觉得复制完后\r\nctrl+x把富文本中的" +
+    "内容保存到剪贴板\r\n再ctrl+v保存到word上\r\n";
+            // 
+            // replace
+            // 
+            this.replace.AutoSize = true;
+            this.replace.Font = new System.Drawing.Font("宋体", 10F);
+            this.replace.Location = new System.Drawing.Point(618, 287);
+            this.replace.Name = "replace";
+            this.replace.Size = new System.Drawing.Size(86, 27);
+            this.replace.TabIndex = 4;
+            this.replace.Text = "批量替换";
+            this.replace.UseVisualStyleBackColor = true;
+            this.replace.Click += new System.EventHandler(this.replace_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(709, 287);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 25);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(566, 287);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 25);
+            this.textBox2.TabIndex = 6;
+            // 
+            // back_to_last
+            // 
+            this.back_to_last.AutoSize = true;
+            this.back_to_last.Font = new System.Drawing.Font("宋体", 10F);
+            this.back_to_last.Location = new System.Drawing.Point(18, 21);
+            this.back_to_last.Name = "back_to_last";
+            this.back_to_last.Size = new System.Drawing.Size(189, 27);
+            this.back_to_last.TabIndex = 8;
+            this.back_to_last.Text = "返回上一步操作(未完)";
+            this.back_to_last.UseVisualStyleBackColor = true;
+            this.back_to_last.Visible = false;
+            this.back_to_last.Click += new System.EventHandler(this.back_to_last_Click);
+            // 
+            // labelStatusDescription
+            // 
+            this.labelStatusDescription.Location = new System.Drawing.Point(12, 353);
+            this.labelStatusDescription.Name = "labelStatusDescription";
+            this.labelStatusDescription.Size = new System.Drawing.Size(542, 20);
+            this.labelStatusDescription.TabIndex = 10;
+            this.labelStatusDescription.Text = "源码访问https://gitee.com/renux/MySectionClip";
+            this.labelStatusDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkFormFirst
+            // 
+            this.checkFormFirst.AutoSize = true;
+            this.checkFormFirst.Checked = true;
+            this.checkFormFirst.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkFormFirst.Location = new System.Drawing.Point(563, 231);
+            this.checkFormFirst.Name = "checkFormFirst";
+            this.checkFormFirst.Size = new System.Drawing.Size(164, 19);
+            this.checkFormFirst.TabIndex = 11;
+            this.checkFormFirst.Text = "该窗口是否锚定最前";
+            this.checkFormFirst.UseVisualStyleBackColor = true;
+            this.checkFormFirst.CheckedChanged += new System.EventHandler(this.checkFormFirst_CheckedChanged);
+            // 
+            // controlFilter
+            // 
+            this.controlFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.controlFilter.DropDownWidth = 200;
+            this.controlFilter.FormattingEnabled = true;
+            this.controlFilter.Items.AddRange(new object[] {
+            "格式过滤器:多个\\n-->空格",
+            "格式过滤器:多个\\n-->空",
+            "格式过滤器:多个\\n-->一个\\n"});
+            this.controlFilter.Location = new System.Drawing.Point(746, 258);
+            this.controlFilter.Name = "controlFilter";
+            this.controlFilter.Size = new System.Drawing.Size(120, 23);
+            this.controlFilter.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(560, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 31);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "自动过滤器规则控制:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(566, 322);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 25);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            // 
+            // OpacitySetting
+            // 
+            this.OpacitySetting.AutoSize = true;
+            this.OpacitySetting.Font = new System.Drawing.Font("宋体", 10F);
+            this.OpacitySetting.Location = new System.Drawing.Point(618, 320);
+            this.OpacitySetting.Name = "OpacitySetting";
+            this.OpacitySetting.Size = new System.Drawing.Size(103, 27);
+            this.OpacitySetting.TabIndex = 15;
+            this.OpacitySetting.Text = "透明度设置";
+            this.OpacitySetting.UseVisualStyleBackColor = true;
+            this.OpacitySetting.Click += new System.EventHandler(this.Opacity_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 381);
+            this.Controls.Add(this.OpacitySetting);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.controlFilter);
+            this.Controls.Add(this.checkFormFirst);
+            this.Controls.Add(this.labelStatusDescription);
+            this.Controls.Add(this.back_to_last);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.replace);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.copy);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Opacity = 0.8D;
+            this.Text = "MySectionClip1.2";
+            this.TopMost = true;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Closed);
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMain_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button copy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button replace;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button back_to_last;
+        private System.Windows.Forms.Label labelStatusDescription;
+        private System.Windows.Forms.CheckBox checkFormFirst;
+        private System.Windows.Forms.ComboBox controlFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button OpacitySetting;
+    }
+}
+
---
+++ MySectionClip/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySectionClip
+{
+    public partial class Form1 : Form
+    {
+        //private Stack<string> p = new Stack<string>();
+        //const int StackDeepNumber = 50;
+        HotKeys h = new HotKeys();
+
+        // SetClipboardViewer 用于往观察链中添加一个窗口句柄，这个窗口就可成为观察链中的一员了，返回值指向下一个观察者
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern IntPtr SetClipboardViewer(IntPtr hwnd);
+
+        //ChangeClipboardChain删除由hwnd指定的观察链成员，这是一个窗口句柄，第二个参数hWndNext是观察链中下一个窗口的句柄
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern IntPtr ChangeClipboardChain(IntPtr hwnd, IntPtr hWndNext);
+
+        //SendMessage 发送消息
+        [System.Runtime.InteropServices.DllImport("user32")]
+        private static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        //Clipboard内容变化消息
+        const int WM_DRAWCLIPBOARD = 0x308;
+        //Clipboard观察链变化消息
+        const int WM_CHANGECBCHAIN = 0x30D;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.controlFilter.SelectedIndex = 0;
+        }
+        //注意把以下两个函数映射到相应事件
+        //存放观察链中下一个窗口句柄
+        IntPtr NextClipHwnd;
+        private void Form_Load(object sender, EventArgs e)
+        {
+            //获得观察链中下一个窗口句柄
+            NextClipHwnd = SetClipboardViewer(this.Handle);
+
+            //全局热键注册
+            h.Regist(this.Handle, (int)HotKeys.HotkeyModifiers.Control, Keys.Q, TopMostSetting);
+            h.Regist(this.Handle, (int)HotKeys.HotkeyModifiers.Control, Keys.X, Copy);
+
+        }
+        private void Form_Closed(object sender, FormClosedEventArgs e)
+        {
+            //从观察链中删除本观察窗口
+            ChangeClipboardChain(this.Handle, NextClipHwnd);
+            //将变动消息WM_CHANGECBCHAIN消息传递到下一个观察链中的窗口
+            SendMessage(NextClipHwnd, WM_CHANGECBCHAIN, this.Handle, NextClipHwnd);
+
+            //全局热键注销
+            h.UnRegist(this.Handle, TopMostSetting);
+            h.UnRegist(this.Handle, Copy);
+        }
+        //重载WndProc方法，监视剪切板，并处理，并把剪切板变化的消息发送给下一个观察者
+        protected override void WndProc(ref System.Windows.Forms.Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    //将WM_DRAWCLIPBOARD消息传递到下一个观察链中的窗口
+                    SendMessage(NextClipHwnd, m.Msg, m.WParam, m.LParam);
+
+                    //获取Clipboard内容，并处理
+                    //IDataObject iData = Clipboard.GetDataObject();
+                    ClipboardContentProcessing();
+                    h.ProcessHotKey(m);
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    h.ProcessHotKey(m);
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+
+        private void StringFilteringRules(ref string text, string s_in, string s_out) //用于批量替换
+        {
+            if (s_in == "") return;
+            StringBuilder sb = new StringBuilder(text);
+            sb.Replace(s_in, s_out);
+            text = sb.ToString();
+        }
+        private string CharacterFilteringRules(string text, string s_in, string s_out)
+        {
+            if (s_in == "") return "";
+            if (text == "" || text == " " || text == "\n") return "";
+            return Regex.Replace(text, s_in + "+", s_out);//s_in是正则字符串
+        }
+        private string CharacterFilteringRulesShell(string s)//用于对复制得到内容中换行符的处理
+        {
+            StringBuilder sb = new StringBuilder(s);
+            for (int i = 0; i < sb.Length; i++) if (sb[i].Equals('\r')) sb[i] = '\n';//注意win下的特殊换行符
+            string s1 = sb.ToString();
+            if (controlFilter.Text == "" || controlFilter.SelectedIndex == 0) return CharacterFilteringRules(s1, "\n", " ");
+            else if (controlFilter.SelectedIndex == 1) return CharacterFilteringRules(s1, "\n", "");
+            else if (controlFilter.SelectedIndex == 2) return CharacterFilteringRules(s1, "\n", "\n");
+            else return s1;
+        }
+        private void GetNumberForPags()//给每个有效信息段增加序号
+        {
+
+        }
+        private void LabelStatusDescription(int a)//状态变化说明
+        {
+            if (a == 1) labelStatusDescription.Text = "富文本窗口上的信息已复制到剪贴板";
+            else if (a == 0) labelStatusDescription.Text = "状态说明";
+            else if (a == 2) labelStatusDescription.Text = "富文本窗口上的信息已清空";
+            else if (a == 3) labelStatusDescription.Text = "富文本窗口上的信息已批量替换";
+            else if (a == 4) labelStatusDescription.Text = "窗口的置顶状态已改变";
+            else labelStatusDescription.Text = "状态说明";
+        }
+        //剪贴板内容获取并处理
+        private void ClipboardContentProcessing()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            string s1 = (string)iData.GetData(DataFormats.Text);
+            string s = CharacterFilteringRulesShell((string)iData.GetData(DataFormats.Text));
+            richTextBox1.Text = richTextBox1.Text + s + '\n' + '\n';
+
+            //注意返回上一步操作:
+            MyStackOperation();
+        }
+        private void TopMostSetting()
+        {
+            if (this.TopMost == false) this.TopMost = true;
+            else this.TopMost = false;
+            LabelStatusDescription(4);
+        }
+        //快捷键映射
+        private void FrmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 二级组合键
+            if (e.Control && e.KeyCode == Keys.Q)         //Ctrl+q
+            {
+                TopMostSetting();
+            }
+            //问题:如何使用本程序操纵剪贴板:比如ctrl+b同时完成了复制当前内容到剪贴板和复制剪贴板内容到其他平台(剪贴板本身功能)?
+            else if (e.Control && e.KeyCode == Keys.X)//复制当前内容到剪贴板
+            {
+                copy_Click(this, EventArgs.Empty);
+            }
+
+            else;
+        }
+        //一个栈操作:如果栈满后push不进去因该怎么做
+        private void MyStackOperation()
+        {
+            //p.Push(richTextBox1.Text);
+            //if (p.Count > StackDeepNumber)
+            //{
+            //    //如果栈满后,应该遗忘以前的操作只记录新的
+            //    //一种操作:把栈倒出来,保留最上面的一半值,从新塞回去
+            //    string[] strArr = p.ToArray();
+            //    p.Clear();
+            //    for (int i = strArr.Length / 2; i >= 0; i--) p.Push(strArr[i]);
+
+            //}
+            return;
+
+        }
+        private void Clear()
+        {
+            richTextBox1.Text = "";
+
+            MyStackOperation();
+
+            LabelStatusDescription(2);
+        }
+
+        private void clear_Click(object sender, EventArgs e)//清空
+        {
+            Clear();
+        }
+        private void Copy()
+        {
+            Clipboard.SetDataObject(richTextBox1.Text + "");
+
+            MyStackOperation();
+            richTextBox1.Text = "";
+
+            LabelStatusDescription(1);
+        }
+        private void copy_Click(object sender, EventArgs e)//复制
+        {
+            Copy();
+        }
+
+        private void replace_Click(object sender, EventArgs e)//批量替换
+        {
+            string s = richTextBox1.Text;
+            StringFilteringRules(ref s, textBox1.Text, textBox2.Text);
+            richTextBox1.Text = s;
+
+            MyStackOperation();
+
+            LabelStatusDescription(3);
+        }
+
+        private void back_to_last_Click(object sender, EventArgs e)//定义一个深度为StackDeepNumber的栈保存富文本窗口的text
+        {
+           // if (p.Count > 0) richTextBox1.Text = p.Pop();
+
+            LabelStatusDescription(0);
+        }
+
+        private void checkFormFirst_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkFormFirst.Checked == true) this.TopMost = true;
+            else this.TopMost = false;
+        }
+
+        private void Opacity_Click(object sender, EventArgs e)
+        {
+            this.Opacity = (double)(numericUpDown1.Value / 100);
+        }
+    }
+
+
+    public class HotKeys
+    {
+        //引入系统API
+        [DllImport("user32.dll")]
+        static extern bool RegisterHotKey(IntPtr hWnd, int id, int modifiers, Keys vk);
+        [DllImport("user32.dll")]
+        static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+
+        int keyid = 10;     //区分不同的快捷键
+        Dictionary<int, HotKeyCallBackHanlder> keymap = new Dictionary<int, HotKeyCallBackHanlder>();   //每一个key对于一个处理函数
+        public delegate void HotKeyCallBackHanlder();
+
+        //组合控制键
+        public enum HotkeyModifiers
+        {
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Win = 8
+        }
+
+        //注册快捷键
+        public void Regist(IntPtr hWnd, int modifiers, Keys vk, HotKeyCallBackHanlder callBack)
+        {
+            int id = keyid++;
+            if (!RegisterHotKey(hWnd, id, modifiers, vk))
+            {
+                //退出并输出注册失败
+                //throw new Exception("注册失败！");
+                Environment.Exit(0);
+            }
+            keymap[id] = callBack;
+        }
+
+        // 注销快捷键
+        public void UnRegist(IntPtr hWnd, HotKeyCallBackHanlder callBack)
+        {
+            foreach (KeyValuePair<int, HotKeyCallBackHanlder> var in keymap)
+            {
+                if (var.Value == callBack)
+                {
+                    UnregisterHotKey(hWnd, var.Key);
+                    return;
+                }
+            }
+        }
+
+        // 快捷键消息处理
+        public void ProcessHotKey(Message m)
+        {
+            if (m.Msg == 0x312)
+            {
+                int id = m.WParam.ToInt32();
+                HotKeyCallBackHanlder callback;
+                if (keymap.TryGetValue(id, out callback))
+                    callback();
+            }
+        }
+    }
+}
